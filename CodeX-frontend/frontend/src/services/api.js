9495759c9ab7328ff02bb41a3869628b2171025d
Bashar Ahmad Khan <150275763+basharahmadkhan10@users.noreply.github.com>
2026-03-31 03:41:50 +0530

    Update api.js

--- CodeX-frontend/frontend/src/services/api.js
+++ CodeX-frontend/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://codex-arena-backend-90y5.onrender.com",
+  baseURL: "https://codex-arena-backend-90y5.onrender.com/api/v1",
   withCredentials: true,
 });
 
